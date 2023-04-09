f65b5edcf6c7b9c9981ca7f80298ab09c76267f4
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2023-04-09 16:21:48 -0400

    registro de pago de mensaludad mejorado

--- src/helpers/createMonthlyPaymentRegister.js
+++ src/helpers/createMonthlyPaymentRegister.js
@@ -1,74 +1,34 @@
 const { getMontlyPaymentsInfo } = require("../controllers/monthlyPayment.controller")
-const Joi = require("joi")
-const { isRecordExisting } = require("../controllers/register.controller")
-const { getAbono, abonoRegisterExist } = require("../controllers/abono.controller")
+
+
 
 async function createMonthlyPaymentRegister(register) {
 
-  const schema = Joi.object({
-    register_code: Joi.string().required(),
-    student_code: Joi.string().required(),
-    tutor_code: Joi.string().required(),
-    payment: Joi.number().required(),
-    monthly_price: Joi.number().required(),
-    cash: Joi.string().required(),
-    operation_number: Joi.string().required(),
-    operation_date: Joi.date().required(),
-    months: Joi.number().required(),
-    user: Joi.number().required(),
-    updatedAT: Joi.date().required()
-  });
-
-  const { error, valor } = schema.validate(register);
-
-  if (error) {
-    console.log(error.details[0].message)
-    return { error: "los datos no son correctos" };
-  }
-  const { student_code, tutor_code, payment, monthly_price, cash, operation_number, operation_date, months, register_code, user, updatedAT } = register;
-
-  //revisa que el registro no este registrado
-  const recordExist = await isRecordExisting(register_code);
-  if (recordExist) {
-    return { error: "el registo ya existe" }
-  }
-
-  //obtiene el abono del tutor
-  const abono = await getAbono(tutor_code);
+  const { student_code, months, updatedAT } = register;
 
   //obtiene la información de los meses pagados por el estudiante
   const monthly_payment_info = await getMontlyPaymentsInfo(student_code);
 
-  // multiplica el precio del mes por los meses a pagar, asi se obtiene la deuda total
-  const requieredPayment = monthly_price * months;
-
-  //suma el abono almacenado al dinero suministrado y se obtiene la cantidad de dinero total que dispone el tutor
-  const money = Number.parseFloat(payment) + Number.parseFloat(abono);
-
-  //se resta el dinero total menos la deuda
-  const total = money - requieredPayment;
-
-  //la diferencia del abono en la sentencia SQL si el dinero no alcanza para pagar los meses solicitados
-  const abonoDiference = {
-    add: `+${payment}`,
-    less: `-${payment}`
-  };
 
   //obtiene el ultimo pago realizado
   const last_PaymentDone = monthly_payment_info?.last_payment ?? getCurrentDate();
-  //const last_PaymentDone ="2022/11/5"
+
   //lista de meses que se deben
   const pending_payments = getDateArray(last_PaymentDone)
 
-  let last_payment = "";
 
   //si se pagan mas meses que los meses que se deben
   if (months > pending_payments.length) {
     //se calcula la diferencia entre los meses que se deben y los meses que se pagan
     let diferenceMonths = months - pending_payments.length;
-   
+
+    if (diferenceMonths == 1) {
+      diferenceMonths = Number.parseInt(months)
+    }
+
     //a la fecha actual se le suma la difetencia de meses
-    let currentDate = new Date();
+    let currentDate = new Date(last_PaymentDone);
+
     currentDate.setMonth(currentDate.getMonth() + diferenceMonths);
     last_payment = formatDate(currentDate);
 
@@ -76,7 +36,7 @@ async function createMonthlyPaymentRegister(register) {
   } else if (pending_payments.length > months) {
 
     //se eliminan los meses mas viejos
-    for (let i = 0; i < months; i++) {
+    for (let i = 0; i < months - 1; i++) {
       pending_payments.shift();
     }
     //se obtiene la fecha mas vieja que queda
@@ -85,80 +45,20 @@ async function createMonthlyPaymentRegister(register) {
     //la ultima opcion, que sean iguales
   } else {
     let currentDate = new Date();
-    currentDate.setMonth(currentDate.getMonth() + 1);
+    currentDate.setMonth(currentDate.getMonth());
     last_payment = formatDate(currentDate)
   }
 
 
-    /**
-     * si total < 0 implica que no alcanzó el dinero, por lo tanto ese dinero se abona
-     * el abono es igual al money
-     * 
-     * si el total es mayor o igual a 0, implica que el dinero si alcanzó
-     * 
-     *por lo tanto se acturaliza la fecha de pago
-     la fecha esta almacenada en last_payment y para el rollback se almacena en last_PaymentDone
-
-     el abono en este caso es lo que está guardado en la variable total.
-     * 
-     */
+  insertion_query = `INSERT INTO monthly_payments (student_code, last_payment, updatedAT) VALUES ('${student_code}', '${last_payment}', '${updatedAT}') ` +
+    `ON DUPLICATE KEY UPDATE last_payment = '${last_payment}', updatedAT = '${updatedAT}'; `;
 
 
-  let insertion_query = "";
-  let rollback_query = "";
-//verifica si existe un registro en la tabla abonos para el tutor
-  const abonoExist = await abonoRegisterExist(tutor_code);
- // console.log({abonoExist: abonoExist == true});
-  //si el total de dinero no alcanza para pagar los meses, entonces se abona
-  if (total < 0) {
+  rollback_query = `UPDATE monthly_payments SET last_payment = '${last_PaymentDone}', updatedAT = '${updatedAT}' WHERE student_code = '${student_code}'; `
 
-    if(abonoExist){
-      insertion_query = `UPDATE abonos SET  abono = abono + ${abonoDiference.add}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}'`;
 
-      rollback_query = `UPDATE abonos SET  abono = abono + ${abonoDiference.less}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}'`;
-
-    }else{
-      insertion_query = `INSERT INTO abonos (tutor_code, abono, updatedAT) VALUES ('${tutor_code}', '${money}', '${updatedAT}');`;
-
-      rollback_query = `UPDATE abonos SET  abono = abono + ${money}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}'`;
-    }
-
-  } else {
-
-    let abn = Math.abs(abono - total);
-
-    if(abonoExist){
-      insertion_query = `UPDATE monthly_payments SET last_payment = '${last_payment}', updatedAT = '${updatedAT}' WHERE student_code = '${student_code}'; ` +
-      `UPDATE abonos SET  abono = abono + ${abn}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}'`;
-
-    }else{
-      insertion_query = `INSERT INTO monthly_payments (student_code, last_payment, updatedAT) VALUES ('${student_code}', '${last_payment}', '${updatedAT}'); ` +
-      `INSERT INTO abonos (tutor_code, abono, updatedAT) VALUES ('${tutor_code}', '${total}', '${updatedAT}')`;
-    }
-
-    rollback_query = `UPDATE monthly_payments SET last_payment = '${last_PaymentDone}', updatedAT = '${updatedAT}' WHERE student_code = '${student_code}'; ` +
-    `UPDATE abonos SET  abono = abono - ${abn}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}'`;
-
-  }
 
   return {
-    register_code: register_code,
-    user: user,
-    description: "Pago de mensualidad de estudiante",
-    date: "03/29/2023 09:03:25",
-    type: "3",
-    metadata: {
-      student_code,
-      payment,
-      cash,
-      operation_number,
-      monthly_price,
-      operation_date,
-      updatedAT,
-      tutor_code,
-      months,
-      pending_payments
-    },
     insertion_query,
     rollback_query
   }
@@ -167,18 +67,6 @@ async function createMonthlyPaymentRegister(register) {
 
 module.exports = createMonthlyPaymentRegister;
 
-function monthsToPay(fecha) {
-  // Convertir la fecha suministrada en un objeto Date
-  const fechaSuministrada = new Date(fecha);
-  // Obtener la fecha actual
-  const fechaActual = new Date();
-
-  // Calcular la diferencia de meses
-  const diferenciaMeses = (fechaActual.getFullYear() - fechaSuministrada.getFullYear()) * 12 + (fechaActual.getMonth() - fechaSuministrada.getMonth());
-
-  // Retornar el valor de la diferencia de meses
-  return diferenciaMeses;
-}
 
 
 function getCurrentDate() {
@@ -195,11 +83,11 @@ function getDateArray(dateString) {
   const startMonth = new Date(startDate.getFullYear(), startDate.getMonth(), 15);
   const currentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 15);
 
-  if (startMonth < currentMonth) {
+  if (startMonth <= currentMonth) {
     const dateArray = [];
     let nextDate = new Date(startDate);
 
-    while (nextDate < currentMonth) {
+    while (nextDate <= currentMonth) {
       nextDate.setMonth(nextDate.getMonth() + 1);
       dateArray.push(formatDate(nextDate));
     }
--- src/joinners/registerMonthlyPayment.joiner.js
+++ src/joinners/registerMonthlyPayment.joiner.js
@@ -1,12 +1,20 @@
 const Joi = require("joi");
 const createMonthlyPaymentRegister = require("../helpers/createMonthlyPaymentRegister");
 const sequelize = require("../SQL/Sequelize/connection");
-const { createRegister } = require("../controllers/register.controller")
+const { getAbono } = require("../controllers/abono.controller");
+const { isRecordExisting } = require("../controllers/register.controller")
+const { createRegister } = require("../controllers/register.controller");
+
+
 
 async function monthlyPayment(req, res) {
+    let PAYMENT_DONE = 0;
+    let MONTHS = 0;
+    let MONTHLY_PRICE = 0;
+    let pays = [];
 
     const data = req.body;
-
+    //revisa que se recibe un array de al menos un elemento
     const schema = Joi.object({
         data: Joi.array().min(1).required()
     })
@@ -19,41 +27,154 @@ async function monthlyPayment(req, res) {
         return res.status(400).json({ error: error.details[0].message });
     }
 
-    const promises = data.map(register => createMonthlyPaymentRegister(register))
-    const registers = await Promise.all(promises);
+    //valida cada una de los indices del array
+    for (let register of data) {
+        const schema = Joi.object({
+            register_code: Joi.string().required(),
+            student_code: Joi.string().required(),
+            tutor_code: Joi.string().required(),
+            payment: Joi.number().required(),
+            monthly_price: Joi.number().required(),
+            cash: Joi.string().required(),
+            operation_number: Joi.string().required(),
+            operation_date: Joi.date().required(),
+            months: Joi.number().required(),
+            user: Joi.number().required(),
+            updatedAT: Joi.date().required()
+        });
 
+        const { error, valor } = schema.validate(register);
 
-
-
-
-
-
-    for (register of registers) {
-
-        if (register.error) {
-            res.status(500).json(register);
-            return;
-        }
-        const transaction = await sequelize.transaction();
-        try {
-            await createRegister(register, transaction);
-            await sequelize.query(register.insertion_query, { transaction });
-            await transaction.commit();
-        } catch (error) {
-            console.log(error)
-            await transaction.rollback();
-            res.status(500).json(registers)
+        if (error) {
+            console.log(error.details[0].message)
+            res.status(500).json({ error: "los datos no son correctos" })
             return
         }
-
     }
 
-    return res.status(200).json(registers);
+    const recordExist = await isRecordExisting(data[0].register_code);
+    if (recordExist) {
+        res.status(500).json({ error: "el registo ya existe" })
+        return
+    }
+
+
+    for (let register of data) {
+        //la cantidad de meses a pagar
+        MONTHS += Number.parseInt(register.months);
+
+        student = {};
+        student[register.student_code] = register.months;
+        pays.push(student)
+
+    }
+    //la fecha
+    let updatedAT = data[0].updatedAT;
+
+    //el pago hecho
+    PAYMENT_DONE = data[0].payment;
+
+    //el precio del mes
+    MONTHLY_PRICE = data[0].monthly_price;
+
+    //la cantidad que se debe pagar
+    let required_payment = Number.parseFloat(MONTHLY_PRICE) * Number.parseFloat(MONTHS);
+
+    //el tutor
+    let tutor_code = data[0].tutor_code;
+
+    //El abono que tiene el tutor almacenado
+    let stored_abono = await getAbono(tutor_code);
+
+    //
+    let subtotal = Number.parseFloat(stored_abono) + Number.parseFloat(PAYMENT_DONE);
+
+    //el total despuyes de sumar el abono + deposito - el dinero a pagar
+    let total = subtotal - Number.parseFloat(required_payment);
+
+
+    let abono_diference = total - stored_abono;
+    let insertion_query = "";
+    let rollback_query = "";
+
+    if (total >= 0) {
+
+        insertion_query = `INSERT INTO abonos (tutor_code, abono, updatedAT) VALUES ('${tutor_code}', '${total}', '${updatedAT}') ` +
+            `ON DUPLICATE KEY UPDATE abono = abono + ${abono_diference}, updatedAT = '${updatedAT}'; `;
+
+        rollback_query = `UPDATE abonos SET abono = abono - ${abono_diference}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}'; `
+
+        const promises = data.map(register => createMonthlyPaymentRegister(register))
+        const registers = await Promise.all(promises);
+
+        for (register of registers) {
+
+            insertion_query += register.insertion_query;
+            rollback_query += register.rollback_query
+
+        }
+
+
+    } else {
+
+        insertion_query = `INSERT INTO abonos (tutor_code, abono, updatedAT) VALUES ('${tutor_code}', '${subtotal}', '${updatedAT}') ` +
+            `ON DUPLICATE KEY UPDATE abono = abono + ${PAYMENT_DONE}, updatedAT = '${updatedAT}' `;
+
+        rollback_query = `Update abonos SET abono = abono - ${PAYMENT_DONE}, updatedAT = '${updatedAT}' WHERE tutor_code = '${tutor_code}';`
+    }
+
+    let register_data = {
+        register_code: data[0].register_code,
+        user: data[0].user,
+        description: "Pago de mensualidad",
+        date: getCurrentDate(),
+        type: 3,
+        metadata: {
+            pays, 
+            monts: MONTHS,
+            required_payment,
+            stored_abono,
+            payment_done : PAYMENT_DONE,
+            total_money : subtotal
+        },
+        insertion_query,
+        rollback_query,
+        updatedAT
+    }
+
+  
+
+    const transaction = await sequelize.transaction();
+
+    try {
+        //await createRegister(register_data, transaction);
+        await sequelize.query(register_data.insertion_query, { transaction });
+
+    } catch (error) {
+        console.log(error)
+        await transaction.rollback();
+        res.status(500).json({error: "Ocurrió un error al intentar insrtar el registro"})
+        return
+    }
+
+
+
+    await transaction.commit();
+    return res.status(200).json(register_data);
 
 }
 
 module.exports = monthlyPayment;
 
 
-
-
+function getCurrentDate() {
+    const fecha = new Date();
+    const mes = fecha.getMonth() + 1;
+    const dia = fecha.getDate();
+    const anio = fecha.getFullYear();
+    const horas = fecha.getHours();
+    const minutos = fecha.getMinutes();
+    const segundos = fecha.getSeconds();
+    const fechaActual = `${mes}/${dia}/${anio} ${horas}:${minutos}:${segundos}`;
+    return fechaActual;
+}
